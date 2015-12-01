0e3239527760b477bc778d508fa850fd622be3bd
Alex <grievea86@gmail.com>
2015-11-30 21:16:42 -0600

    Added more categories, got ratings to finally work

---
+++ app/inputs/datetime_picker_input.rb
@@ -0,0 +1,22 @@
+class DatetimePickerInput < DatePickerInput
+  private
+
+  # def display_pattern
+  #   I18n.t('datepicker.dformat', default: '%d/%m/%Y') + ' ' +
+  #       I18n.t('timepicker.dformat', default: '%R')
+  # end
+  def display_pattern
+    I18n.t('datepicker.dformat', default: '%m/%d/%Y') + ' ' +
+        I18n.t('timepicker.dformat', default: '%R')
+  end
+
+  # def picker_pattern
+  #   I18n.t('datepicker.pformat', default: 'DD/MM/YYYY') + ' ' +
+  #       I18n.t('timepicker.pformat', default: 'HH:mm')
+  # end
+
+   def picker_pattern
+    I18n.t('datepicker.pformat', default: 'MM/DD/YYYY') + ' ' +
+        I18n.t('timepicker.pformat', default: 'HH:mm')
+  end
+end
